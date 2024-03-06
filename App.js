67b6e11c619a235f48d95d072046cbb1ecf48694
ZofearZ <64070203@it.kmitl.ac.th>
2024-03-06 21:03:03 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import NewHouse from "./screens/NewHouse";
+import PetLost from "./screens/PetLost";
+import UploadPets from "./screens/UploadPets";
+import Test from "./screens/Test";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Test />
+    // <NewHouse />
+    // <PetLost />
+    // <UploadPets />
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
